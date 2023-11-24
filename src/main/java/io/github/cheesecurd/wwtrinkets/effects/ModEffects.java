6cb138fa8f68e6dd667bbb420e40eab26808b835
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-11-23 21:26:56 -0500

    Effects

--- src/main/java/io/github/cheesecurd/wwtrinkets/effects/ModEffects.java
+++ src/main/java/io/github/cheesecurd/wwtrinkets/effects/ModEffects.java
@@ -3,25 +3,21 @@ package io.github.cheesecurd.wwtrinkets.effects;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModEffects extends StatusEffects
 {
-	public static final StatusEffect FALL_DAMAGE =
-			Registry.register(Registry.STATUS_EFFECT,
-					1,
-					"fall",
-					(new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0)));
-	public static final StatusEffect SUFFOCATION =
-			Registry.register(Registry.STATUS_EFFECT,
-					2,
-					"suffocate",
-					(new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0)));
-	public static final StatusEffect STARVATION =
-			Registry.register(Registry.STATUS_EFFECT,
-					3,
-					"starvation",
-					(new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0)));
+	public static final StatusEffect FALL_DAMAGE = registerEffect("fall",
+			new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0));
+	public static final StatusEffect SUFFOCATION = registerEffect("suffocate",
+			new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0));
+	public static final StatusEffect STARVATION = registerEffect("starvation",
+			new ScriptEffect(StatusEffectCategory.BENEFICIAL, 0));
 
 	public static void registerEffects() {}
+	private static StatusEffect registerEffect(String name, StatusEffect effect)
+	{
+		return Registry.register(Registry.STATUS_EFFECT, new Identifier("wwtrinkets", name), effect);
+	}
 }
